3355f202b6c82ee510795f568d1a6befe5300a97
MD. Gazzali Fahim <fahim9778@gmail.com>
2019-10-13 16:09:03 +0600

    [COMPLETED] miniLens Project (3.5/4)

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # miniLens
-Android app called 'miniLens' which will be a mini version of Google Lens. 
-Like the Google Lens app, which should have the ability to identify objects, humans, emotions based on machine learning algorithm. We would also like to integrate text analysis or OCR in this app.
+A sample app that aims to clone the functionality of Google Lens using [Firebase ML Kit](https://firebase.google.com/products/ml-kit/) APIs.
+Android app called 'miniLens' which is a mini version of Google Lens. Like the Google Lens app, it has the ability to identify objects, humans, emotions based on machine learning algorithm. We will like to integrate OCR and Landmark Detection in the future development of this app.
+
+# Setup Instructions
+
+1. Clone the project
+2. Add it to your Firebase Console
+3. Magic !!!
+
+Built with ❤️ by [@fahim9778](https://github.com/fahim9778)
---
+++ app/src/main/java/io/github/the_dagger/mlkit/ImageLabelActivity.kt
@@ -0,0 +1,114 @@
+package io.github.the_dagger.mlkit
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.support.design.widget.BottomSheetBehavior
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_bottom_sheet.*
+
+
+class ImageLabelActivity : AppCompatActivity() {
+
+    private var itemsList: ArrayList<Any> = ArrayList()
+    private lateinit var itemAdapter: ImageLabelAdapter
+
+    private lateinit var sheetBehavior: BottomSheetBehavior<*>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        rvLabel.layoutManager = LinearLayoutManager(this)
+        btnRetry.setOnClickListener {
+            if (cameraView.visibility == View.VISIBLE) showPreview() else hidePreview()
+        }
+
+        sheetBehavior = BottomSheetBehavior.from(bottomLayout)
+        sheetBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {}
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                fab_take_photo.animate().scaleX(1 - slideOffset).scaleY(1 - slideOffset).setDuration(0).start()
+            }
+        })
+
+        fab_take_photo.setOnClickListener {
+            fabProgressCircle.show()
+            cameraView.captureImage { cameraKitImage ->
+                // Get the Bitmap from the captured shot
+                getLabelsFromDevice(cameraKitImage.bitmap)
+                runOnUiThread {
+                    showPreview()
+                    imagePreview.setImageBitmap(cameraKitImage.bitmap)
+                }
+            }
+        }
+    }
+
+    private fun getLabelsFromDevice(bitmap: Bitmap) {
+        val image = FirebaseVisionImage.fromBitmap(bitmap)
+        val detector = FirebaseVision.getInstance().visionLabelDetector
+        itemsList.clear()
+        detector.detectInImage(image)
+                .addOnSuccessListener {
+                    // Task completed successfully
+                    fabProgressCircle.hide()
+                    itemsList.addAll(it)
+                    itemAdapter = ImageLabelAdapter(itemsList,false)
+                    rvLabel.adapter = itemAdapter
+                    sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED)
+                }
+                .addOnFailureListener {
+                    // Task failed with an exception
+                    fabProgressCircle.hide()
+                    Toast.makeText(baseContext,"Sorry, something went wrong!",Toast.LENGTH_SHORT).show()
+                }
+    }
+
+    private fun getLabelsFromClod(bitmap: Bitmap) {
+        val image = FirebaseVisionImage.fromBitmap(bitmap)
+        val detector = FirebaseVision.getInstance()
+                .visionCloudLabelDetector
+        itemsList.clear()
+        detector.detectInImage(image)
+                .addOnSuccessListener {
+                    // Task completed successfully
+                    fabProgressCircle.hide()
+                    itemsList.addAll(it)
+                    itemAdapter = ImageLabelAdapter(itemsList,true)
+                    rvLabel.adapter = itemAdapter
+                    sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED)
+                }
+                .addOnFailureListener {
+                    // Task failed with an exception
+                    fabProgressCircle.hide()
+                    Toast.makeText(baseContext,"Sorry, something went wrong!",Toast.LENGTH_SHORT).show()
+                }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        cameraView.start()
+    }
+
+    override fun onPause() {
+        cameraView.stop()
+        super.onPause()
+    }
+
+    private fun showPreview() {
+        framePreview.visibility = View.VISIBLE
+        cameraView.visibility = View.GONE
+    }
+
+    private fun hidePreview() {
+        framePreview.visibility = View.GONE
+        cameraView.visibility = View.VISIBLE
+    }
+
+}
